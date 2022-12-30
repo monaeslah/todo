3bbb96b8c7d030e9ce6ba2425a66382e9ddbeb63
Arash <arash.aghajani@morressier.com>
2022-12-30 11:58:25 +0100

    style

--- src/App.js
+++ src/App.js
@@ -2,26 +2,17 @@ import React, { useState } from "react";
 import Count from "./componants/counter/index";
 import Todo from "./componants/list/index";
 
-
-const App = () => { 
+const App = () => {
   const [darkMode, changeMode] = useState(true);
-  
-
-
 
   function moodChange() {
     changeMode(!darkMode);
   }
 
-
-   return (
+  return (
     <span className={darkMode ? "theme--default" : "theme--dark"}>
       <div className="App ">
-        <Todo
-        moodChange={moodChange}
-         darkMode={darkMode} 
-      />
-      
+        <Todo moodChange={moodChange} darkMode={darkMode} />
       </div>
     </span>
   );
